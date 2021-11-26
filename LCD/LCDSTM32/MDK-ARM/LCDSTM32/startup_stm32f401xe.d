d2d50f8bac4867c183156f948b290ecbac6a7aad
Huynh-Thi-Ngoc-Khanh <63081068+Huynh-Thi-Ngoc-Khanh@users.noreply.github.com>
2021-11-26 22:37:08 +0700

    del

---
+++ LCD/LCDSTM32/MDK-ARM/LCDSTM32/startup_stm32f401xe.d
@@ -0,0 +1,1 @@
+lcdstm32\startup_stm32f401xe.o: startup_stm32f401xe.s
---
+++ LCD/LCDSTM32/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LCDSTM32' 
+ * Target:  'LCDSTM32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
